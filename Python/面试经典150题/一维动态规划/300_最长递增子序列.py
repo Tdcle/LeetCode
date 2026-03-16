26c900a31fef5f8e8947bc2ef268b1b2960973b6
wlc <2642268493@qq.com>
2026-03-16 14:00:27 +0800

    2026/3/16

---
+++ Python/面试经典150题/一维动态规划/300_最长递增子序列.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1]*n
+        for i in range(1,n):
+            for j in range(i):
+                if nums[i]>nums[j]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        return max(dp)
+
+print(Solution().lengthOfLIS([1,3,6,7,9,4,10,5,6]))
